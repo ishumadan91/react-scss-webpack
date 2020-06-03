161094f093136ec01cc329ac465bec952b67ec6e
Ishu Madan <ishu.madan91@gmail.com>
2020-06-03 07:43:52 +0530

    Remove obselete packages

--- src/app.js
+++ src/app.js
@@ -1,18 +1,12 @@
 import React from 'react';
-import { hot } from 'react-hot-loader';
 import { BrowserRouter, Route, Link } from "react-router-dom";
 import './app.scss';
-import { createStore } from 'redux'
-import { Provider } from 'react-redux'
 import Home from './components/Home';
 import About from './components/About';
-import Users from './components/Users';
-import reducers from './redux/reducers';
-
-const store = createStore(reducers, {list: null})
 
 const App = () => (
-    <Provider store={store}>
+    <div>
+        <p>Header</p>
         <BrowserRouter>
         <div>
             <nav>
@@ -23,19 +17,15 @@ const App = () => (
                 <li>
                 <Link to="/about/">About</Link>
                 </li>
-                <li>
-                <Link to="/users/">Users</Link>
-                </li>
             </ul>
             </nav>
 
             <Route path="/" exact component={Home} />
             <Route path="/about/" component={About} />
-            <Route path="/users/" component={Users} />
         </div>
         </BrowserRouter>
-    </Provider>
-    
+        <p>Footer</p>
+    </div>
 );
 
 export default App;
